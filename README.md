034da170b5a1ca0a9ecc53ca1a5149f9962512a5
Varoun P <commits@varoun.com>
2013-06-05 16:30:12 +0530

    Initial import.

---
+++ README.md
@@ -0,0 +1,32 @@
+# Defroute
+
+Defroute is a minimalist routing library for Hunchentoot. It dispatches requests to handler
+functions based on the HTTP request method and the resource URI. Handler functions must accept
+one argument (the Hunchentoot request object), and return the response.
+
+## Usage
+
+The code has comments that serves as the canonical documentation for defroute.
+
+## License
+
+Copyright (c) 2013, Varoun P
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted
+provided that the following conditions are met:
+         1. Redistributions of source code must retain the above copyright notice, this list of
+            conditions and the following disclaimer.
+         2. Redistributions in binary form must reproduce the above copyright notice, this list
+            of conditions and the following disclaimer in the documentation and/or other
+            materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES [INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION] HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT [INCLUDING NEGLIGENCE OR
+OTHERWISE] ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
---
+++ defroute.lisp
@@ -0,0 +1,81 @@
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: CL-USER; Base: 10 -*-
+
+;;;; Author: varoun (Varoun P)
+
+;;;; Simple routing library for Hunchentoot.
+
+(in-package #:hunchentoot)
+; (declaim #.*optimize-default*)
+
+(defvar *dispatch* '()
+  "A list of routes to match against. The order matters - the first route that matches wins. A
+  route itself is a list of the form (:http-method (:prefix uri) function) or (:http-method
+  (:regex uri) function).")
+
+(defun method-matches-p (method request)
+  "True if the http method in the REQUEST, a hunchentoot `request' object, matches the METHOD
+  (\keyword)."
+  (eql method (request-method request)))
+
+(defun uri-matches-p (uri request)
+  "True if the uri in the REQUEST, a hunchentoot `request' object, matches the URI (\alist) of
+  the form (:prefix uri) or (:regex uri)."
+  (case (first uri)
+    (:prefix 
+     (let ((mismatch (mismatch (script-name request)
+                               (second uri)
+                               :test #'char=)))
+       (or (null mismatch)
+           (>= mismatch (length (second uri))))))
+    (:regex
+     (let ((scanner (create-scanner (second uri))))
+       (scan scanner (script-name request))))))
+
+;;; Hook into the Hunchentoot request dispatch mechanism.
+
+(defclass defroute-acceptor (acceptor)
+  ()
+  (:documentation "The acceptor for the defroute request routing framework."))
+
+(defclass defroute-ssl-acceptor (defroute-acceptor ssl-acceptor)
+  ()
+  (:documentation "Add support for SSL connections to the defroute framework."))
+
+(defmethod acceptor-dispatch-request ((acceptor defroute-acceptor) request)
+  (loop 
+     for (method uri handler) in *dispatch*
+     when (and (method-matches-p method request)
+               (uri-matches-p uri request))
+     return (funcall handler request)
+     finally (call-next-method)))
+
+
+;; Usage:
+;; CL-USER> (in-package :hunchentoot)
+;; #<Package "HUNCHENTOOT">
+;; TBNL> (start (make-instance 'defroute-acceptor :port 8080))
+;; #<DEFROUTE-ACCEPTOR (host *, port 8080)>
+;; TBNL> (defun say-get (request)
+;;         (setf (content-type*) "text/plain")
+;;         "You used GET on the resource.")
+;; ;Compiler warnings :
+;; ;   In SAY-GET: Unused lexical variable REQUEST
+;; SAY-GET
+;; TBNL> (defun say-post (request)
+;;         (setf (content-type*) "text/plain")
+;;         "You used POST on the resource.")
+;; ;Compiler warnings :
+;; ;   In SAY-POST: Unused lexical variable REQUEST
+;; SAY-POST
+;; TBNL> (push '(:GET (:prefix "/v1/someresource") say-get) *dispatch*)
+;; ((:GET (:PREFIX "/v1/someresource") SAY-GET))
+;; TBNL> (push '(:POST (:prefix "/v1/someresource") say-post) *dispatch*)
+;; ((:POST (:PREFIX "/v1/someresource") SAY-POST) (:GET (:PREFIX "/v1/someresource") SAY-GET))
+;; TBNL>
+
+;; In a shell
+;; $ curl http://localhost:8080/v1/someresource
+;; You used GET on the resource.
+;; $ curl -d "q=foo" http://localhost:8080/v1/someresource
+;; You used POST on the resource.
+;; $
